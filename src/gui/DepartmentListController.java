284dd4518935b1a05e312d47d193247cfe516779
joaolummertz1@gmail.com <joaolummertz1@gmail.com>
2020-10-27 04:14:24 -0300

    Implemented DepartmentService findAll method and TableView updates

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentService departmentService;
+	
 	@FXML
 	private TableView<Department> tableViewDepartments;
 	@FXML
@@ -24,9 +30,24 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button buttonNew;
 	
+	private ObservableList<Department> observableList;
+	
 	@FXML
 	public void onButtonNewAction() {
 		System.out.println("onButtonNewAction");
+	}
+	
+	public void setDepartmentService(DepartmentService departmentService) {
+		this.departmentService = departmentService;
+	}
+	
+	public void updateTableView() {
+		if (departmentService == null) {
+			throw new IllegalStateException("DepartmentService is null");
+		}
+		List<Department> list = departmentService.findAll();
+		observableList = FXCollections.observableArrayList(list);
+		tableViewDepartments.setItems(observableList);
 	}
 	
 	@Override
@@ -41,5 +62,4 @@ public class DepartmentListController implements Initializable {
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartments.prefHeightProperty().bind(stage.heightProperty());
 	}
-
 }
